5ef985ccbbecb9244471cf58cdd0e541b000f5ad
Becky <becky.ruan524@gmail.com>
2022-01-23 11:23:31 -0800

    created employee repository, added get all employees method

---
+++ EmployeeManagementService/EmployeeManagementService.Infrastructure/Persistence/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagementService.Infrastructure.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementService.Infrastructure.Persistence
+{
+    public class EmployeeRepository
+    {
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            using (var context = new RofSchedulerContext())
+            {
+                return await context.Employees.ToListAsync();
+            }
+        }
+    }
+}
