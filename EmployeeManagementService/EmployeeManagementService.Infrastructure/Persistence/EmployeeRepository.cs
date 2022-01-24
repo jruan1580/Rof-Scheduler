e48ae5235db80b7428ef4116887a535bbb130fb0
Becky <becky.ruan524@gmail.com>
2022-01-23 16:06:18 -0800

    create employee method

--- EmployeeManagementService/EmployeeManagementService.Infrastructure/Persistence/EmployeeRepository.cs
+++ EmployeeManagementService/EmployeeManagementService.Infrastructure/Persistence/EmployeeRepository.cs
@@ -17,5 +17,24 @@ namespace EmployeeManagementService.Infrastructure.Persistence
                 return await context.Employees.Select(e => new Employee() { FirstName  = e.FirstName, LastName = e.LastName, Role = e.Role, Username = e.Username, Active = e.Active }).ToListAsync();
             }
         }
+
+        public async Task CreateEmployee( string firstName, string lastName, string username, byte[] password, string role)
+        {
+            using (var context = new RofSchedulerContext())
+            {
+                var employee = new Employee()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Username = username,
+                    Password = password,
+                    Role = role
+                };
+
+                context.Employees.Add(employee);
+
+                await context.SaveChangesAsync();
+            }
+        }
     }
 }
