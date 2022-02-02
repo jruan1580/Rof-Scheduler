91bc35e7224550d4e6fccc0a00e7fd04dbfd1ca7
Becky <becky.ruan524@gmail.com>
2022-02-01 18:27:41 -0800

    added automapper, created mapper folder, added mapper class

---
+++ EmployeeManagementService/EmployeeManagementService.Domain/Mappers/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CoreEmployee = EmployeeManagementService.Domain.Models.Employee;
+using DbEmployee = EmployeeManagementService.Infrastructure.Persistence.Entities.Employee;
+
+
+namespace EmployeeManagementService.Domain.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<DbEmployee, CoreEmployee>();
+        }
+    }
+}
