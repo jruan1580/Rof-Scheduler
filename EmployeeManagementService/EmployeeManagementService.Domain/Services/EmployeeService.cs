757e95caf5a529880f115804d097f827f37270ec
Becky <becky.ruan524@gmail.com>
2022-02-01 18:36:25 -0800

    extracted interface from EmployeeRepository, edits to EmployeeService

--- EmployeeManagementService/EmployeeManagementService.Domain/Services/EmployeeService.cs
+++ EmployeeManagementService/EmployeeManagementService.Domain/Services/EmployeeService.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using EmployeeManagementService.Infrastructure.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EmployeeManagementService.Domain.Services
 {
-    class EmployeeService
+    public class EmployeeService
     {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
     }
 }
