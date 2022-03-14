31e4825def5c4723929f6a5b64ffb566d9e1fdfe
Becky <becky.ruan524@gmail.com>
2022-03-14 15:16:22 -0700

    dto mapper

---
+++ EmployeeManagementService/EmployeeManagementService.API/DTO/EmployeeDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementService.API.DTO
+{
+    public class EmployeeDTO
+    {
+        public long Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Ssn { get; set; }
+
+        public string Username { get; set; }
+
+        public byte[] Password { get; set; }
+
+        public string Role { get; set; }
+
+        public bool IsLocked { get; set; }
+
+        public short FailedLoginAttempts { get; set; }
+
+        public bool TempPasswordChanged { get; set; }
+
+        public bool Status { get; set; }
+
+        public bool? Active { get; set; }
+    }
+}
