3431fe0d2a9f342e1c16faac96a81533fc1c1da8
Becky <becky.ruan524@gmail.com>
2022-04-08 13:23:12 -0700

    some employee controller test

--- EmployeeManagementService/EmployeeManagementService.Test/Controller/EmployeeControllerTest.cs
+++ EmployeeManagementService/EmployeeManagementService.Test/Controller/EmployeeControllerTest.cs
@@ -1,10 +1,160 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EmployeeManagementService.API.Controllers;
+using EmployeeManagementService.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace EmployeeManagementService.Test.Controller
 {
-    class EmployeeControllerTest
+    [TestFixture]
+    public class EmployeeControllerTest
     {
+        private Mock<IEmployeeService> _employeeService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _employeeService = new Mock<IEmployeeService>();
+        }
+
+        [Test]
+        public async Task UpdateEmployeeInformation_Success()
+        {
+            var updateEmployee = new API.DTO.EmployeeDTO()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Ssn = "123-45-6789",
+                Username = "jdoe",
+                Role = "Employee",
+                Address = new API.DTO.AddressDTO { AddressLine1 = "123 Abc St", AddressLine2 = "", City = "Oakland", State = "CA", ZipCode = "12345" }
+            };
+
+            var controller = new EmployeeController(_employeeService.Object);
+
+            var response = await controller.UpdateEmployeeInformation(updateEmployee);
+
+            Assert.NotNull(response);
+            Assert.AreEqual(response.GetType(), typeof(OkResult));
+
+            var ok = (OkResult)response;
+
+            Assert.AreEqual(ok.StatusCode, 200);
+        }
+
+        [Test]
+        public async Task UpdateEmployeeInformation_InternalServerError()
+        {
+            var updateEmployee = new API.DTO.EmployeeDTO()
+            {
+                Id = 0,
+                FirstName = "",
+                LastName = "",
+                Ssn = "",
+                Username = "",
+                Role = "",
+                Address = new API.DTO.AddressDTO { AddressLine1 = "", AddressLine2 = "", City = "", State = "", ZipCode = "" }
+            };
+
+            var controller = new EmployeeController(_employeeService.Object);
+
+            var response = await controller.UpdateEmployeeInformation(updateEmployee);
+
+            Assert.NotNull(response);
+            Assert.AreEqual(response.GetType(), typeof(ObjectResult));
+
+            var obj = (ObjectResult)response;
+
+            Assert.AreEqual(obj.StatusCode, 500);
+        }
+
+        [Test]
+        public async Task GetEmployeeById_Success()
+        {
+            _employeeService.Setup(e => e.GetEmployeeById(It.IsAny<long>()))
+                .ReturnsAsync(new Domain.Models.Employee()
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Ssn = "123-45-6789",
+                    Username = "jdoe",
+                    Password = new byte[100],
+                    Role = "Employee",
+                    IsLocked = false,
+                    FailedLoginAttempts = 0,
+                    TempPasswordChanged = false,
+                    Status = false,
+                    Active = true
+                });
+
+            var controller = new EmployeeController(_employeeService.Object);
+
+            var response = await controller.GetEmployeeById(1);
+
+            Assert.NotNull(response);
+            Assert.AreEqual(response.GetType(), typeof(OkObjectResult));
+
+            var okObj = (OkObjectResult)response;
+
+            Assert.AreEqual(okObj.StatusCode, 200);
+        }
+
+        [Test]
+        public async Task GetEmployeeById_InternalServerError()
+        {
+            _employeeService.Setup(e => e.GetEmployeeById(It.IsAny<long>()))
+                .ReturnsAsync((Domain.Models.Employee)null);
+
+            var controller = new EmployeeController(_employeeService.Object);
+
+            var response = await controller.GetEmployeeById(1);
+
+            Assert.NotNull(response);
+            Assert.AreEqual(response.GetType(), typeof(ObjectResult));
+
+            var obj = (ObjectResult)response;
+
+            Assert.AreEqual(obj.StatusCode, 500);
+        }
+
+        [Test]
+        public async Task GetEmployeeByUsername_Success()
+        {
+            
+        }
+
+        [Test]
+        public async Task GetEmployeeByUsername_InternalServerError()
+        {
+
+        }
+
+        [Test]
+        public async Task EmployeeLogin_Success()
+        {
+            
+        }
+
+        [Test]
+        public async Task EmployeeLogin_InternalServerError()
+        {
+
+        }
+
+        [Test]
+        public async Task EmployeeLogout_Success()
+        {
+            
+        }
+
+        [Test]
+        public async Task EmployeeLogout_InternalServerError()
+        {
+
+        }
     }
 }
