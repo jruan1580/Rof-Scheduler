1ea30ae750324ec9fca2bdde6161c78c54c801b1
Becky <becky.ruan524@gmail.com>
2022-05-04 20:36:00 -0700

    update info edits

--- rof-scheduler/src/AccountSettings/index.js
+++ rof-scheduler/src/AccountSettings/index.js
@@ -1,9 +1,22 @@
-import { Tab, Nav, Row, Col, Form, Button, Container } from "react-bootstrap";
-import { getEmployeeById } from "../SharedServices/employeeManagementService";
+import {
+  Tab,
+  Nav,
+  Row,
+  Col,
+  Form,
+  Button,
+  Container,
+  Spinner,
+  Alert,
+} from "react-bootstrap";
+import {
+  getEmployeeById,
+  updateEmployeeInformation,
+} from "../SharedServices/employeeManagementService";
 import { useEffect, useState } from "react";
 
 function AccountSettings() {
-  let ee = {
+  const ee = {
     firstName: "",
     lastName: "",
     ssn: "",
@@ -19,19 +32,72 @@ function AccountSettings() {
   };
 
   const [employee, setEmployee] = useState(ee);
+  const [loading, setLoading] = useState(false);
+  const [updateErrMsg, setUpdateErrMsg] = useState("");
+  const [inputError, setInputError] = useState(false);
 
   useEffect(() => {
     (async function () {
       try {
         const emp = await getEmployeeById();
         setEmployee(emp);
-        console.log({ emp });
       } catch (e) {
         alert("Failed to fetch employee info... Try Again");
         console.log(e.message);
       }
     })();
   }, []);
+
+  const handleSubmit = (submitEvent) => {
+    submitEvent.preventDefault();
+
+    const firstName = submitEvent.target.firstName.value;
+    const lastName = submitEvent.target.lastName.value;
+    const ssn = submitEvent.target.ssn.value;
+    const role = submitEvent.target.role.value;
+    const username = submitEvent.target.username.value;
+    const addressLine1 = submitEvent.target.address1.value;
+    const addressLine2 = submitEvent.target.address2.value;
+    const city = submitEvent.target.city.value;
+    const state = submitEvent.target.state.value;
+    const zipCode = submitEvent.target.zip.value;
+
+    setLoading(true);
+    (async function () {
+      try {
+        await updateEmployeeInformation(
+          firstName,
+          lastName,
+          ssn,
+          role,
+          username,
+          addressLine1,
+          addressLine2,
+          city,
+          state,
+          zipCode
+        );
+
+        setEmployee({
+          firstName: submitEvent.target.firstName.value,
+          lastName: submitEvent.target.lastName.value,
+          ssn: submitEvent.target.ssn.value,
+          role: submitEvent.target.role.value,
+          username: submitEvent.target.username.value,
+          addressLine1: submitEvent.target.address1,
+          addressLine2: submitEvent.target.address2.value,
+          city: submitEvent.target.address.city.value,
+          state: submitEvent.target.address.state.value,
+          zipCode: submitEvent.target.address.zip.value,
+        });
+        setUpdateErrMsg("");
+      } catch (e) {
+        setUpdateErrMsg(e.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  };
 
   return (
     <>
@@ -56,12 +122,17 @@ function AccountSettings() {
             <Col sm={9}>
               <Tab.Content>
                 <Tab.Pane eventKey="accountSettings">
-                  <Form>
+                  <Form noValidate onSubmit={handleSubmit}>
+                    {updateErrMsg !== "" && (
+                      <Alert variant="danger">{updateErrMsg}</Alert>
+                    )}
+
                     <Row className="mb-3">
                       <Form.Group as={Col} md="4">
                         <Form.Label>First name</Form.Label>
                         <Form.Control
                           required
+                          name="firstName"
                           type="text"
                           placeholder="First name"
                           defaultValue={employee.firstName}
@@ -72,6 +143,7 @@ function AccountSettings() {
                         <Form.Label>Last name</Form.Label>
                         <Form.Control
                           required
+                          name="lastName"
                           type="text"
                           placeholder="Last name"
                           defaultValue={employee.lastName}
@@ -84,7 +156,7 @@ function AccountSettings() {
                           required
                           type="text"
                           placeholder="Username"
-                          disabled
+                          name="username"
                           defaultValue={employee.username}
                         />
                       </Form.Group>
@@ -93,9 +165,15 @@ function AccountSettings() {
                     <Row className="mb-3">
                       <Form.Group as={Col} md="4">
                         <Form.Label>Role</Form.Label>
-                        <Form.Select required type="select" placeholder="Role">
-                          <option value="admin">Admin</option>
-                          <option value="employee">Employee</option>
+                        <Form.Select
+                          required
+                          type="select"
+                          placeholder="Role"
+                          name="role"
+                          defaultValue={employee.role}
+                        >
+                          <option value="Administrator">Administrator</option>
+                          <option value="Employee">Employee</option>
                         </Form.Select>
                       </Form.Group>
 
@@ -103,6 +181,7 @@ function AccountSettings() {
                         <Form.Label>SSN</Form.Label>
                         <Form.Control
                           required
+                          name="ssn"
                           type="text"
                           placeholder="SSN"
                           defaultValue={employee.ssn}
@@ -126,6 +205,7 @@ function AccountSettings() {
                         <Form.Label>Address Line 1</Form.Label>
                         <Form.Control
                           required
+                          name="address1"
                           type="text"
                           placeholder="Address Line 1"
                           defaultValue={employee.address.addressLine1}
@@ -135,6 +215,7 @@ function AccountSettings() {
                         <Form.Label>Address Line 2</Form.Label>
                         <Form.Control
                           type="text"
+                          name="address2"
                           placeholder="Address Line 2"
                           defaultValue={employee.address.addressLine2}
                         />
@@ -146,6 +227,7 @@ function AccountSettings() {
                         <Form.Label>City</Form.Label>
                         <Form.Control
                           required
+                          name="city"
                           type="text"
                           placeholder="City"
                           defaultValue={employee.address.city}
@@ -155,6 +237,7 @@ function AccountSettings() {
                         <Form.Label>State</Form.Label>
                         <Form.Control
                           required
+                          name="state"
                           type="text"
                           placeholder="State"
                           defaultValue={employee.address.state}
@@ -164,6 +247,7 @@ function AccountSettings() {
                         <Form.Label>Zipcode</Form.Label>
                         <Form.Control
                           required
+                          name="zip"
                           type="text"
                           placeholder="Zipcode"
                           defaultValue={employee.address.zipCode}
@@ -171,13 +255,27 @@ function AccountSettings() {
                       </Form.Group>
                     </Row>
 
-                    <Button
-                      variant="outline-primary"
-                      type="submit"
-                      style={{ marginTop: "0.5rem" }}
-                    >
-                      Save
-                    </Button>
+                    {loading && (
+                      <Button variant="primary" disabled>
+                        <Spinner
+                          as="span"
+                          animation="grow"
+                          size="sm"
+                          role="status"
+                          aria-hidden="true"
+                        />
+                        Loading...
+                      </Button>
+                    )}
+                    {!loading && (
+                      <Button
+                        variant="outline-primary"
+                        type="submit"
+                        style={{ marginTop: "0.5rem" }}
+                      >
+                        Save
+                      </Button>
+                    )}
                   </Form>
                 </Tab.Pane>
 
--- rof-scheduler/src/SharedServices/employeeManagementService.js
+++ rof-scheduler/src/SharedServices/employeeManagementService.js
@@ -68,3 +68,52 @@ export const getEmployeeById = async function () {
 
   return await response.json();
 };
+
+export const updateEmployeeInformation = async function (
+  firstName,
+  lastName,
+  ssn,
+  role,
+  username,
+  addressLine1,
+  addressLine2,
+  city,
+  state,
+  zipCode
+) {
+  const baseUrl = process.env.REACT_APP_EMPLOYEE_MANAGEMENT_BASE_URL;
+  const data = {
+    firstName,
+    lastName,
+    ssn,
+    role,
+    username,
+    address: {
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      zipCode,
+    },
+  };
+
+  role = localStorage.getItem("role");
+  const url =
+    role.toLowerCase() === "administrator"
+      ? baseUrl + "/admin/info"
+      : baseUrl + "/employee/info";
+
+  var response = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    credentials: "include",
+  });
+
+  if (response.status !== 200) {
+    var errMsg = await response.text();
+    throw new Error(errMsg);
+  }
+};
