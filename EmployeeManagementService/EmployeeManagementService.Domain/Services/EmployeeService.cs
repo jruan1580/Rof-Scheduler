195a985e7dc58013c718b9b1ce59ac8193cef22a
Jason <jruan1580@gmail.com>
2022-05-28 15:41:28 -0700

    fix postman tests

--- EmployeeManagementService/EmployeeManagementService.Domain/Services/EmployeeService.cs
+++ EmployeeManagementService/EmployeeManagementService.Domain/Services/EmployeeService.cs
@@ -278,6 +278,7 @@ namespace EmployeeManagementService.Domain.Services
             }
 
             employee.IsLocked = true;
+            employee.FailedLoginAttempts = (short)attempts;
 
             await _employeeRepository.UpdateEmployee(employee);
         }
