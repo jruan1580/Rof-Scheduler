d9b58a4d2758dd43fc7d2a1bd5080fcac492814c
Becky <becky.ruan524@gmail.com>
2022-06-29 16:41:39 -0700

    edits to client ui

--- rof-scheduler/src/ClientManagement/index.js
+++ rof-scheduler/src/ClientManagement/index.js
@@ -1,72 +1,167 @@
-import { useEffect, useState } from 'react';
-import { Form, Button, Row, Col, Alert, Pagination } from 'react-bootstrap';
-import AddUserModal from '../SharedComponents/AddUser';
-import GenericUserTable from '../SharedComponents/UserTable';
-import { getAllClients } from '../SharedServices/clientManagementService';
+import { useEffect, useState } from "react";
+import { Form, Button, Row, Col, Alert, Pagination } from "react-bootstrap";
+import AddUserModal from "../SharedComponents/AddUser";
+import GenericUserTable from "../SharedComponents/UserTable";
+import UpdateUserModal from "../SharedComponents/UpdateUser";
+import LoadingModal from "../SharedComponents/LoadingModal";
+import {
+  getAllClients,
+  resetClientLockStatus,
+} from "../SharedServices/clientManagementService";
 
-function ClientManagement(){
-    const [clients, setClients] = useState([]);
-    const [currPage, setCurrPage] = useState(1);
-    const [errorMessage, setErrorMessage] = useState(undefined);
-    const [keyword, setKeyword] = useState("");
-    const [totalPages, setTotalPages] = useState(0);
-    const [showAddModal, setShowAddModal] = useState(false);
+function ClientManagement() {
+  const [clients, setClients] = useState([]);
+  const [currPage, setCurrPage] = useState(1);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [keyword, setKeyword] = useState("");
+  const [totalPages, setTotalPages] = useState(0);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [currClientToUpdate, setCurrClientToUpdate] = useState(undefined);
+  const [showLoadingModal, setShowLoadingModal] = useState(false);
 
-    useEffect(() =>{        
-        (async function(){
-            try{
-                const clientByPage = await getAllClients(currPage, 10, keyword);
-                
-                setClients(clientByPage.clients);
-                setTotalPages(clientByPage.totalPages);
-            }catch(e){
-                setErrorMessage(e.message);                
-            }
-        })();
+  useEffect(() => {
+    (async function () {
+      try {
+        const clientByPage = await getAllClients(currPage, 10, keyword);
 
-    }, [currPage, keyword]);
+        setClients(clientByPage.clients);
+        setTotalPages(clientByPage.totalPages);
+      } catch (e) {
+        setErrorMessage(e.message);
+      }
+    })();
+  }, [currPage, keyword]);
 
-    const search = (searchEvent) => {
-        searchEvent.preventDefault();
-        const searchTerm = (searchEvent.target.searchClient.value === undefined) ? "" : searchEvent.target.searchClient.value;
-        setKeyword(searchTerm);
-        setCurrPage(1);              
+  const search = (searchEvent) => {
+    searchEvent.preventDefault();
+    const searchTerm =
+      searchEvent.target.searchClient.value === undefined
+        ? ""
+        : searchEvent.target.searchClient.value;
+    setKeyword(searchTerm);
+    setCurrPage(1);
+  };
+
+  const reloadAfterThreeSeconds = () => {
+    setTimeout(() => window.location.reload(), 3000);
+  };
+
+  const resetClientLock = (id) => {
+    (async function () {
+      try {
+        setShowLoadingModal(true);
+        await resetClientLockStatus(id);
+        for (var i = 0; i < clients.length; i++) {
+          if (clients[i].id !== id) {
+            continue;
+          }
+
+          clients[i].isLocked = false;
+        }
+
+        setClients(clients);
+      } catch (e) {
+        setErrorMessage(e.message);
+      } finally {
+        setShowLoadingModal(false);
+      }
+    })();
+  };
+
+  const postUpdateClientAction = (updatedFieldsMap) => {
+    for (var i = 0; i < clients.length; i++) {
+      if (clients[i].id !== updatedFieldsMap.get("id")) {
+        continue;
+      }
+
+      clients[i].fullName =
+        updatedFieldsMap.get("firstName") +
+        " " +
+        updatedFieldsMap.get("lastName");
+      clients[i].firstName = updatedFieldsMap.get("firstName");
+      clients[i].lastName = updatedFieldsMap.get("lastName");
+      clients[i].emailAddress = updatedFieldsMap.get("email");
+      clients[i].username = updatedFieldsMap.get("username");
+      clients[i].phoneNumber = updatedFieldsMap.get("phoneNumber");
+      clients[i].secPhoneNum = updatedFieldsMap.get("secPhoneNum");
+      clients[i].address = updatedFieldsMap.get("address");
     }
 
-    const reloadAfterThreeSeconds = () => {
-        setTimeout(() => window.location.reload(), 3000);
-    }
+    setClients(clients);
+  };
 
-    return(
-            <>
-                <h1>Client Management</h1><br/>
-                <AddUserModal userType='Client' show={showAddModal} handleHide={() => setShowAddModal(false)} handleUserAddSuccess={reloadAfterThreeSeconds}/>
-                <Row>
-                    <Form  onSubmit={search}>
-                        <Row className="align-items-center">
-                            <Col lg={4}>            
-                                <Form.Control name="searchClient" id="searchClient" placeholder="Search client by name, email..etc." />
-                            </Col>
-                            <Col lg={6}>
-                                <Button type="submit">Search</Button>
-                            </Col>
-                            <Col lg={2}>
-                                <Button onClick={() => setShowAddModal(true)}>Add Client</Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Row><br/>
-                {errorMessage !== undefined && <Alert variant='danger'>{errorMessage}</Alert>}
-                <Row>
-                    <GenericUserTable users={clients}/>   
-                </Row>
-                <Pagination>
-                    {currPage != 1 && <Pagination.Prev onClick={() => setCurrPage(currPage - 1)}/> }
-                    {currPage != totalPages && <Pagination.Next onClick={() => setCurrPage(currPage + 1)}/> }
-                </Pagination>
-                          
-            </>
-    );
+  const loadUpdateModal = (user) => {
+    setCurrClientToUpdate(user);
+
+    setShowUpdateModal(true);
+  };
+
+  const closeUpdateModal = () => {
+    setCurrClientToUpdate(undefined);
+
+    setShowUpdateModal(false);
+  };
+
+  return (
+    <>
+      <h1>Client Management</h1>
+      <br />
+      <AddUserModal
+        userType="Client"
+        show={showAddModal}
+        handleHide={() => setShowAddModal(false)}
+        handleUserAddSuccess={reloadAfterThreeSeconds}
+      />
+      <UpdateUserModal
+        user={currClientToUpdate}
+        userType="Client"
+        show={showUpdateModal}
+        hideModal={closeUpdateModal}
+        postUpdateAction={postUpdateClientAction}
+      />
+      <LoadingModal show={showLoadingModal} />
+      <Row>
+        <Form onSubmit={search}>
+          <Row className="align-items-center">
+            <Col lg={4}>
+              <Form.Control
+                name="searchClient"
+                id="searchClient"
+                placeholder="Search client by name, email..etc."
+              />
+            </Col>
+            <Col lg={6}>
+              <Button type="submit">Search</Button>
+            </Col>
+            <Col lg={2}>
+              <Button onClick={() => setShowAddModal(true)}>Add Client</Button>
+            </Col>
+          </Row>
+        </Form>
+      </Row>
+      <br />
+      {errorMessage !== undefined && (
+        <Alert variant="danger">{errorMessage}</Alert>
+      )}
+      <Row>
+        <GenericUserTable
+          userType="Client"
+          users={clients}
+          resetUserLockStatus={resetClientLock}
+          showUpdateModal={loadUpdateModal}
+        />
+      </Row>
+      <Pagination>
+        {currPage != 1 && (
+          <Pagination.Prev onClick={() => setCurrPage(currPage - 1)} />
+        )}
+        {currPage != totalPages && (
+          <Pagination.Next onClick={() => setCurrPage(currPage + 1)} />
+        )}
+      </Pagination>
+    </>
+  );
 }
 
 export default ClientManagement;
--- rof-scheduler/src/SharedServices/clientManagementService.js
+++ rof-scheduler/src/SharedServices/clientManagementService.js
@@ -47,9 +47,16 @@ export const createClient = async function (
   }
 };
 
-export const getAllClients = async function(page, recPerPage, keyword){
+export const getAllClients = async function (page, recPerPage, keyword) {
   var baseUrl = process.env.REACT_APP_CLIENT_MANAGEMENT_BASE_URL;
-  var url = baseUrl + "/client?page=" + page + "&offset=" + recPerPage + "&keyword=" + keyword;
+  var url =
+    baseUrl +
+    "/client?page=" +
+    page +
+    "&offset=" +
+    recPerPage +
+    "&keyword=" +
+    keyword;
 
   var response = await fetch(url, {
     method: "GET",
@@ -65,4 +72,68 @@ export const getAllClients = async function(page, recPerPage, keyword){
   }
 
   return await response.json();
-}
+};
+
+export const resetClientLockStatus = async function (id) {
+  var baseUrl = process.env.REACT_APP_CLIENT_MANAGEMENT_BASE_URL;
+  var url = baseUrl + "/client/" + id + "/locked";
+
+  var response = await fetch(url, {
+    method: "PATCH",
+    credentials: "include",
+  });
+
+  if (response.status !== 200) {
+    var errMsg = await response.text();
+    throw new Error(errMsg);
+  }
+};
+
+export const updateClientInformation = async function (
+  id,
+  firstName,
+  lastName,
+  username,
+  email,
+  primaryPhoneNum,
+  secondaryPhoneNum,
+  addressLine1,
+  addressLine2,
+  city,
+  state,
+  zipCode
+) {
+  var baseUrl = process.env.REACT_APP_CLIENT_MANAGEMENT_BASE_URL;
+  var data = {
+    id,
+    firstName,
+    lastName,
+    username,
+    emailAddress: email,
+    primaryPhoneNum,
+    secondaryPhoneNum,
+    address: {
+      addressLine1,
+      addressLine2,
+      city,
+      state,
+      zipCode,
+    },
+  };
+
+  var url = baseUrl + "/client/info";
+
+  var response = await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    credentials: "include",
+  });
+
+  if (response.status !== 200) {
+    var errMsg = await response.text();
+    throw new Error(errMsg);
+  }
+};
