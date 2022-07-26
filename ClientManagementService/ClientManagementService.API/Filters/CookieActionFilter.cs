d85e7f61cd6ad35c1aa5290b01e67955f46f23a3
Jason <jruan1580@gmail.com>
2022-07-25 18:46:03 -0700

    bug fixes while dmeoing

--- ClientManagementService/ClientManagementService.API/Filters/CookieActionFilter.cs
+++ ClientManagementService/ClientManagementService.API/Filters/CookieActionFilter.cs
@@ -13,21 +13,21 @@ namespace ClientManagementService.API.Filters
             {
                 var cookie = context.HttpContext.Request.Cookies["X-Access-Token-Admin"];
 
-                context.HttpContext.Response.Cookies.Append("X-Access-Token-Admin", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTimeOffset.Now.AddMinutes(30) });
+                context.HttpContext.Response.Cookies.Append("X-Access-Token-Admin", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTime.Now.AddMinutes(30) });
             }
             
             if (context.HttpContext.Request.Cookies.ContainsKey("X-Access-Token-Employee"))
             {
                 var cookie = context.HttpContext.Request.Cookies["X-Access-Token-Employee"];
 
-                context.HttpContext.Response.Cookies.Append("X-Access-Token-Employee", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTimeOffset.Now.AddMinutes(30) });
+                context.HttpContext.Response.Cookies.Append("X-Access-Token-Employee", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTime.Now.AddMinutes(30) });
             }
 
             if (context.HttpContext.Request.Cookies.ContainsKey("X-Access-Token-Client"))
             {
                 var cookie = context.HttpContext.Request.Cookies["X-Access-Token-Client"];
 
-                context.HttpContext.Response.Cookies.Append("X-Access-Token-Client", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTimeOffset.Now.AddMinutes(30) });
+                context.HttpContext.Response.Cookies.Append("X-Access-Token-Client", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTime.Now.AddMinutes(30) });
             }
         }
     }
--- EmployeeManagementService/EmployeeManagementService.API/Filters/CookieActionFilter.cs
+++ EmployeeManagementService/EmployeeManagementService.API/Filters/CookieActionFilter.cs
@@ -13,14 +13,14 @@ namespace EmployeeManagementService.API.Filters
             {
                 var cookie = context.HttpContext.Request.Cookies["X-Access-Token-Admin"];
 
-                context.HttpContext.Response.Cookies.Append("X-Access-Token-Admin", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTimeOffset.Now.AddMinutes(30) });
+                context.HttpContext.Response.Cookies.Append("X-Access-Token-Admin", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTime.Now.AddMinutes(30) });
             }
             
             if (context.HttpContext.Request.Cookies.ContainsKey("X-Access-Token-Employee"))
             {
                 var cookie = context.HttpContext.Request.Cookies["X-Access-Token-Employee"];
 
-                context.HttpContext.Response.Cookies.Append("X-Access-Token-Employee", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTimeOffset.Now.AddMinutes(30) });
+                context.HttpContext.Response.Cookies.Append("X-Access-Token-Employee", cookie, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.None, Secure = true, Path = "/", Expires = DateTime.Now.AddMinutes(30) });
             }
         }
     }
