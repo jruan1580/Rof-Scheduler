3d67c9a92d3fa907ae2b98f1ae5233d399292df5
Jason <jruan1580@gmail.com>
2022-07-30 15:36:30 -0700

    created vaccine repo and domain model. needed when we query for vax by pet type later

---
+++ ClientManagementService/ClientManagementService.Domain/Mappers/Database/VaccineMapper.cs
@@ -0,0 +1,18 @@
+﻿using CoreVax = ClientManagementService.Domain.Models.Vaccine;
+using DbVax = ClientManagementService.Infrastructure.Persistence.Entities.Vaccine;
+
+namespace ClientManagementService.Domain.Mappers.Database
+{
+    public class VaccineMapper
+    {
+        public static CoreVax ToCorePet(DbVax dbPet)
+        {
+            var coreVax = new CoreVax();
+
+            coreVax.Id = dbPet.Id;
+            coreVax.VaxName = dbPet.VaxName;
+
+            return coreVax;
+        }
+    }
+}
---
+++ ClientManagementService/ClientManagementService.Domain/Models/Vaccine.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientManagementService.Domain.Models
+{
+    public class Vaccine
+    {
+        public short Id { get; set; }
+        public string VaxName { get; set; }
+    }
+}
