fa9bd2490525948ddb2e4c5e7b197bc845cb74c8
Jason <jruan1580@gmail.com>
2022-08-06 13:26:53 -0700

    mocked up the view

--- rof-scheduler/src/PetManagement/AddModal/index.js
+++ rof-scheduler/src/PetManagement/AddModal/index.js
@@ -1,9 +1,22 @@
 import { useState } from "react";
 import { Modal, Form, Row, Col, Button } from "react-bootstrap";
+import Select from 'react-select';
 
 function AddPetModal({show, closeModal}){
-    const [petTypeSelected, setPetTypeSelected] = useState(undefined);
+    const [petTypeSelected, setPetTypeSelected] = useState(1);
 
+    const breed = [
+        { label: 'Golden Retriever', value: 1 },
+        { label: 'Husky', value: 2 }
+      ];
+      
+    const addPet = (e) =>{
+        e.preventDefault();
+
+        console.log(e.target.breed.value);
+        console.log(e.target.dob.value);
+        console.log(e);
+    }
     return(
         <>
             {
@@ -50,16 +63,128 @@ function AddPetModal({show, closeModal}){
                 <Modal
                     show={show}
                     onHide={closeModal}
-                    aria-labelledby="contained-modal-title-vcenter"
                     dialogClassName="add-modal80"
-                    centered
                 >
                     <Modal.Header className="modal-header-color" closeButton>
                         <Modal.Title>
                             Add Pet
                         </Modal.Title>
                     </Modal.Header>
-                    
+                      <Modal.Body>
+                        <Form onSubmit={addPet}>
+                            <h4>Pet Information</h4>
+                            <br />
+
+                            <Row>
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control 
+                                        placeholder="Pet Name"
+                                        name="petName"
+                                    />
+                                </Form.Group>
+                                
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Label>Breed</Form.Label>
+                                    <Select
+                                        name="breed"
+                                        options={breed}
+                                    />                             
+                                </Form.Group>
+
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Label>DOB</Form.Label>
+                                    <Form.Control 
+                                        type="date"
+                                        placeholder="DOB"
+                                        name="dob"
+                                    />
+                                </Form.Group>
+
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Label>Weight</Form.Label>
+                                    <Form.Control 
+                                        type="number"
+                                        placeholder="weight"
+                                        name="weight"
+                                    />
+                                </Form.Group>
+                            </Row><br/>
+                           
+                            <Row>
+                                <Form.Group as={Col} lg={12}>
+                                    <Form.Label>Additional Information (Optional)</Form.Label>
+                                    <Form.Control 
+                                        placeholder="Additional Information"
+                                        name="additionalInfo"
+                                        as="textarea" 
+                                        rows={5}
+                                    />
+                                </Form.Group>
+                            </Row><br/>
+                            
+                            <h4>Vaccines</h4>
+                            <br />
+                            <Row>
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Check
+                                        label="Bordetella"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                    <Form.Check
+                                        label="2"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                </Form.Group>
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Check
+                                        label="Bordetella"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                    <Form.Check
+                                        label="2"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                </Form.Group>
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Check
+                                        label="Bordetella"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                    <Form.Check
+                                        label="2"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                </Form.Group>
+                                <Form.Group as={Col} lg={3}>
+                                    <Form.Check
+                                        label="Bordetella"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                    <Form.Check
+                                        label="2"
+                                        name="group1"
+                                        type="checkbox"
+                                    />
+                                </Form.Group>
+                                
+                            </Row>
+
+                            <br />
+                            <hr></hr>
+                            <Button type="submit" className="float-end">
+                                Create
+                            </Button>
+                        </Form>
+                        
+                    </Modal.Body>
                 </Modal>
             }
             
