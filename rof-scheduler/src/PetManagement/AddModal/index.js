98bc32963d1dceebc29f50ec5c1c590b069cd13e
Jason <jruan1580@gmail.com>
2022-08-06 12:40:54 -0700

    wip - dropdown list modal to select pet type

--- rof-scheduler/src/PetManagement/AddModal/index.js
+++ rof-scheduler/src/PetManagement/AddModal/index.js
@@ -1,6 +1,68 @@
-function AddPetModal(){
+import { useState } from "react";
+import { Modal, Form, Row, Col, Button } from "react-bootstrap";
+
+function AddPetModal({show, closeModal}){
+    const [petTypeSelected, setPetTypeSelected] = useState(undefined);
+
     return(
         <>
+            {
+                //have not selected pet type
+                petTypeSelected === undefined &&
+                <Modal
+                    show={show}
+                    onHide={closeModal}
+                >
+                    <Modal.Header className="modal-header-color" closeButton>
+                        <Modal.Title>
+                            Add Pet
+                        </Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group as={Row}>
+                                <Form.Label column lg={3} >Pet Type:</Form.Label>
+                                <Col lg={9}>
+                                    <Form.Select
+                                        type="select"
+                                        placeholder="petType"
+                                        name="petType"
+                                    >
+                                        <option value="dog">Dog</option>
+                                        <option value="cat">Cat</option>
+                                    </Form.Select>
+                                </Col>                                             
+                            </Form.Group>
+                            <br />
+                            <hr></hr>
+                            <Button type="button" className="float-end">
+                                Next
+                            </Button>
+                        </Form>
+                        
+                    </Modal.Body>
+                    
+                </Modal>
+            }
+            {
+                petTypeSelected !== undefined &&
+                <Modal
+                    show={show}
+                    onHide={closeModal}
+                    aria-labelledby="contained-modal-title-vcenter"
+                    dialogClassName="add-modal80"
+                    centered
+                >
+                    <Modal.Header className="modal-header-color" closeButton>
+                        <Modal.Title>
+                            Add Pet
+                        </Modal.Title>
+                    </Modal.Header>
+                    
+                </Modal>
+            }
+            
         </>
     )
 }
--- rof-scheduler/src/PetManagement/index.js
+++ rof-scheduler/src/PetManagement/index.js
@@ -1,7 +1,40 @@
-function PetManagement(){
+import { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+import AddPetModal from "./AddModal";
+
+function PetManagement({ setLoginState }){
+    const [showAddModal, setShowAddModal] = useState(false);  
+
     return(
         <>
             <h1>Pets Management</h1>
+            <br/>
+            <AddPetModal
+                show={showAddModal}
+                closeModal={() => setShowAddModal(false)}
+            />
+            <Row>
+                <Form>
+                <Row className="align-items-center">
+                    <Col lg={4}>
+                        <Form.Control
+                            name="searchPet"
+                            id="searchPet"
+                            placeholder="Search pets by name and breed"
+                        />
+                    </Col>
+                    <Col lg={6}>
+                        <Button type="submit">Search</Button>
+                    </Col>
+                    <Col lg={2}>
+                        <Button onClick={() => setShowAddModal(true)}>
+                            Add Pet
+                        </Button>
+                    </Col>
+                </Row>
+                </Form>
+            </Row>
         </>
     )
 }
