c1cc8a1a843d8aa2c94ceaaf2543c66aade4a3b6
Jason <jruan1580@gmail.com>
2022-09-09 16:00:26 -0700

    added repo and services to startup

--- PetServiceManagement/PetServiceManagement.API/Startup.cs
+++ PetServiceManagement/PetServiceManagement.API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PetServiceManagement.Domain.BusinessLogic;
+using PetServiceManagement.Infrastructure.Persistence.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,13 @@ namespace PetServiceManagement.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IPetServiceRepository, PetServiceRepository>();
+            services.AddSingleton<IHolidayAndRatesRepository, HolidayAndRatesRepository>();
+
+            services.AddTransient<IDropDownService, DropDownService>();
+            services.AddTransient<IPetServiceManagementService, PetServiceManagementService>();
+            services.AddTransient<IHolidayAndRateService, HolidayAndRateService>();
+
             services.AddControllers();
         }
 
--- PetServiceManagement/PetServiceManagement.Tests/Controllers/HolidayRateControllerTests.cs
+++ PetServiceManagement/PetServiceManagement.Tests/Controllers/HolidayRateControllerTests.cs
@@ -7,7 +7,6 @@ using PetServiceManagement.Domain.BusinessLogic;
 using PetServiceManagement.Domain.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PetServiceManagement.Tests.Controllers
