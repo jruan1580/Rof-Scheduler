09146923610de913f3fd5f15451730cbd1e6e0b1
Becky <becky.ruan524@gmail.com>
2022-10-20 16:41:38 -0700

    added event controller

---
+++ EventsManagementService/EventManagementService.API/Controllers/EventController.cs
@@ -0,0 +1,18 @@
+﻿using EventManagementService.Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagementService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+    }
+}
