298c4f6ec1d5024b97725b7bcfae13229294aca6
Becky <becky.ruan524@gmail.com>
2022-11-28 18:24:52 -0800

    edits to unit test

--- EventsManagementService/EventManagementService.Domain/Models/PetService.cs
+++ EventsManagementService/EventManagementService.Domain/Models/PetService.cs
@@ -3,10 +3,14 @@
     public class PetService
     {
         public short Id { get; set; }
-        
+
         public string ServiceName { get; set; }
-        
+
         public decimal Price { get; set; }
+                
+        public int Duration { get; set; }
+        
+        public string TimeUnit { get; set; }
         
         public string Description { get; set; }
     }
