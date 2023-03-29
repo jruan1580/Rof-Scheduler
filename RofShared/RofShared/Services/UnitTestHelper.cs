09b3dd7db04f0f93ddb9704d75dcfaf92072c165
Jason <jruan1580@gmail.com>
2023-03-28 19:33:06 -0700

    move more functions into our shared repo

---
+++ RofShared/RofShared/Services/UnitTestHelper.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RofShared.StartupInits;
+using System;
+using System.Collections.Generic;
+
+namespace RofShared.Services
+{
+    public static class UnitTestHelper
+    {
+        public static IWebHostBuilder GetWebHostBuilder(Action<IServiceCollection> registeredServices)
+        {
+            var requestPipeline = GetRequestPipeline();
+
+            return new WebHostBuilder()
+                .UseKestrel()
+                .ConfigureServices(registeredServices)
+                .Configure(requestPipeline)
+                .UseUrls("http://localhost");
+        }
+
+        public static Action<IApplicationBuilder> GetRequestPipeline()
+        {
+            Action<IApplicationBuilder> requestPipeline = app =>
+            {
+                app.AddExceptionHandlerForApi();
+
+                app.UseHttpsRedirection();
+
+                app.UseRouting();
+
+                app.UseAuthentication();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            };
+
+            return requestPipeline;
+        }
+
+        public static IConfiguration GetConfiguration()
+        {
+            var tokenConfig = new Dictionary<string, string>();
+            tokenConfig.Add("Jwt:Key", "thisisjustsomerandomlocalkey");
+            tokenConfig.Add("Jwt:Issuer", "localhost.com");
+            tokenConfig.Add("Jwt:Audience", "rof_services");
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(tokenConfig)
+                .Build();
+
+            return configuration;
+        }
+    }
+}
