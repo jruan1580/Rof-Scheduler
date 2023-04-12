547c6db4f405bd3e61426d897d777bbb327e6d4d
Becky <becky.ruan524@gmail.com>
2023-04-11 21:27:05 -0700

    Client retrieval service

---
+++ ClientManagementService/ClientManagementService.Domain/Services/ClientRetrievalService.cs
@@ -0,0 +1,52 @@
+﻿using ClientManagementService.Domain.Mappers.Database;
+using ClientManagementService.Domain.Models;
+using ClientManagementService.Infrastructure.Persistence;
+using ClientManagementService.Infrastructure.Persistence.Filters.Client;
+using RofShared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManagementService.Domain.Services
+{
+    public class ClientRetrievalService : ClientBaseService, IClientRetrievalService
+    {
+        public ClientRetrievalService(IClientRetrievalRepository clientRetrievalRepository)
+            : base(clientRetrievalRepository) { }
+
+        public async Task<ClientsWithTotalPage> GetAllClientsByKeyword(int page, int offset, string keyword)
+        {
+            var result = await _clientRetrievalRepository.GetAllClientsByKeyword(page, offset, keyword);
+
+            var clients = result.Item1;
+            var totalPages = result.Item2;
+
+            var coreClients = clients.Select(c => ClientMapper.ToCoreClient(c)).ToList();
+
+            return new ClientsWithTotalPage(coreClients, totalPages);
+        }
+
+        public async Task<Client> GetClientById(long id)
+        {
+            var client = await GetDbClientById(id);
+
+            return ClientMapper.ToCoreClient(client);
+        }
+
+        public async Task<Client> GetClientByEmail(string email)
+        {
+            var client = await GetDbClientByEmail(email);
+
+            return ClientMapper.ToCoreClient(client);
+        }
+
+        public async Task<Client> GetClientByUsername(string username)
+        {
+            var client = await GetDbClientByUsername(username);
+
+            return ClientMapper.ToCoreClient(client);
+        }
+    }
+}
---
+++ ClientManagementService/ClientManagementService.Domain/Services/IClientRetrievalService.cs
@@ -0,0 +1,13 @@
+﻿using ClientManagementService.Domain.Models;
+using System.Threading.Tasks;
+
+namespace ClientManagementService.Domain.Services
+{
+    public interface IClientRetrievalService
+    {
+        Task<ClientsWithTotalPage> GetAllClientsByKeyword(int page, int offset, string keyword);
+        Task<Client> GetClientByEmail(string email);
+        Task<Client> GetClientById(long id);
+        Task<Client> GetClientByUsername(string username);
+    }
+}
