56b61a87b915bc818650480c4d703eca06b743b6
Jason <jruan1580@gmail.com>
2023-04-13 20:05:50 -0700

    new table for employee payroll reports. detailed version

---
+++ Database/Table Scripts/RofDatamart/dbo.EmployeePayrollDetail.sql
@@ -0,0 +1,22 @@
+USE [RofDatamart];
+GO
+
+IF (EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'EmployeePayrollDetail'))
+BEGIN
+	DROP TABLE dbo.EmployeePayrollDetail;
+END
+
+CREATE TABLE dbo.EmployeePayrollDetail
+(
+	EmployeeId BIGINT NOT NULL,
+	FirstName VARCHAR(25) NOT NULL,
+	LastName VARCHAR(25) NOT NULL,
+	PetServiceId BIGINT NOT NULL,
+	PetServiceName VARCHAR(255) NOT NULL,
+	EmployeePayForService DECIMAL(5,2) NOT NULL,
+	ServiceDuration INT NOT NULL, 
+	ServiceDurationTimeUnit VARCHAR(10) NOT NULL,
+	JobEventId BIGINT NOT NULL,
+	ServiceStartDateTime DATETIME NOT NULL,
+	IsHolidayRate BIT NOT NULL
+);
