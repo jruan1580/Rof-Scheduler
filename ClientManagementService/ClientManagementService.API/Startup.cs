9e0246b3e5280fb9edeb0fe957a2df2f66e480c8
Becky <becky.ruan524@gmail.com>
2023-04-16 10:05:07 -0700

    updated start up

--- ClientManagementService/ClientManagementService.API/Startup.cs
+++ ClientManagementService/ClientManagementService.API/Startup.cs
@@ -28,7 +28,7 @@ namespace ClientManagementService.API
             services.AddSingleton<IPetToVaccinesRepository, PetToVaccinesRepository>();
 
             services.AddTransient<IPasswordService, PasswordService>();
-            services.AddTransient<IClientService, ClientAuthService>();
+            services.AddTransient<IClientAuthService, ClientAuthService>();
             services.AddTransient<IClientRetrievalService, ClientRetrievalService>();
             services.AddTransient<IClientUpsertService, ClientUpsertService>();
             services.AddTransient<IPetService, PetService>();
