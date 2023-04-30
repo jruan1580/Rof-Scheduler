f7ace1123b7366d2d2e9f5d8748351a574f3cf80
Becky <becky.ruan524@gmail.com>
2023-04-29 20:37:01 -0700

    event creator

---
+++ EventsManagementService/EventManagementService.Test/EventCreator.cs
@@ -0,0 +1,67 @@
+﻿using EventManagementService.API.DTO;
+using System;
+using DbEvent = EventManagementService.Infrastructure.Persistence.Entities.JobEvent;
+using DomainEvent = EventManagementService.Domain.Models.JobEvent;
+
+namespace EventManagementService.Test
+{
+    public static class EventCreator
+    {
+        public static DbEvent GetDbEvent()
+        {
+            return new DbEvent()
+            {
+                Id = 1,
+                EmployeeId = 1,
+                PetId = 1,
+                PetServiceId = 1,
+                EventStartTime = DateTime.Today,
+                EventEndTime = DateTime.Today,
+                Completed = false
+            };
+        }
+
+        public static DomainEvent GetDomainEvent()
+        {
+            return new DomainEvent()
+            {
+                Id = 1,
+                EmployeeId = 1,
+                PetId = 1,
+                PetServiceId = 1,
+                EventStartTime = DateTime.Today,
+                Completed = false,
+                Employee = new Domain.Models.Employee()
+                {
+                    Id = 1,
+                    FullName = "John Doe"
+                },
+                Pet = new Domain.Models.Pet()
+                {
+                    Id = 1,
+                    Name = "Layla"
+                },
+                PetService = new Domain.Models.PetService()
+                {
+                    Id = 1,
+                    ServiceName = "Walk"
+                }
+            };
+        }
+
+        public static EventDTO GetEventDTO()
+        {
+            return new EventDTO()
+            {
+                EmployeeId = 1,
+                EmployeeFullName = "John Doe",
+                PetId = 1,
+                PetName = "Dog1",
+                PetServiceId = 1,
+                PetServiceName = "Walk",
+                EventStartTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss"),
+                Completed = false
+            };
+        }
+    }
+}
