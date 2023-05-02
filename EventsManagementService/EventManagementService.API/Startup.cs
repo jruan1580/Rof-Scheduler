20ddaae20e2d90075775378712fc52efb40ee632
Becky <becky.ruan524@gmail.com>
2023-05-01 19:37:37 -0700

    DI of new repo

--- EventsManagementService/EventManagementService.API/Startup.cs
+++ EventsManagementService/EventManagementService.API/Startup.cs
@@ -24,6 +24,7 @@ namespace EventManagementService.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IEventRetrievalRepository, EventRetrievalRepository>();
             services.AddSingleton<IEventRepository, EventRepository>();
 
             services.AddTransient<IEventService, EventService>();
