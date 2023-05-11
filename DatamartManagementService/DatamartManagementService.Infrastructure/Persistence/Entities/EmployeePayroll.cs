5672704b8594f0ff1684f7cdc6caea0031cec119
Becky <becky.ruan524@gmail.com>
2023-05-10 19:01:34 -0700

    scaffold db - payroll entities

---
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/Entities/EmployeePayroll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DatamartManagementService.Infrastructure.Persistence.Entities
+{
+    public partial class EmployeePayroll
+    {
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal EmployeeTotalPay { get; set; }
+        public DateTime PayPeriodStartDate { get; set; }
+        public DateTime PayPeriodEndDate { get; set; }
+    }
+}
---
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/Entities/EmployeePayrollDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DatamartManagementService.Infrastructure.Persistence.Entities
+{
+    public partial class EmployeePayrollDetail
+    {
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PetServiceId { get; set; }
+        public string PetServiceName { get; set; }
+        public decimal EmployeePayForService { get; set; }
+        public bool IsHolidayPay { get; set; }
+        public int ServiceDuration { get; set; }
+        public string ServiceDurationTimeUnit { get; set; }
+        public long JobEventId { get; set; }
+        public DateTime ServiceStartDateTime { get; set; }
+        public DateTime ServiceEndDateTime { get; set; }
+    }
+}
