f27f428b0c6275e04dfc5e5e6e0e3b091f5c596c
Becky <becky.ruan524@gmail.com>
2023-05-16 21:07:31 -0700

    revenue by date repo start

---
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatamartManagementService.Infrastructure.Persistence
+{
+    public class RevenueByDateRepository
+    {
+
+    }
+}
