82d4ece0de23a2dc086bccb20f9026db8e49eec4
Becky <becky.ruan524@gmail.com>
2023-05-17 21:49:25 -0700

    some repo methods for revByDate

--- DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateRepository.cs
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateRepository.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using DatamartManagementService.Infrastructure.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DatamartManagementService.Infrastructure.Persistence
 {
     public class RevenueByDateRepository
     {
+        public async Task AddRevenue(List<RofRevenueByDate> newRevenueByDate)
+        {
+            using var context = new RofDatamartContext();
 
+            context.RofRevenueByDate.AddRange(newRevenueByDate);
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<RofRevenueByDate>> GetRevenueByYear(short year)
+        {
+            using var context = new RofDatamartContext();
+
+            var yearlyRevenue = await context.RofRevenueByDate.Where(r => r.RevenueYear == year).ToListAsync();
+
+            return yearlyRevenue;
+        }
     }
 }
