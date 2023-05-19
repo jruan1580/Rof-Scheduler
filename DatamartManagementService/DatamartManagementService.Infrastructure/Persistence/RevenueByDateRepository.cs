182581fe205a984b91d683a6fdc40b59544cbb72
Becky <becky.ruan524@gmail.com>
2023-05-18 19:07:37 -0700

    completed revenue by date repo

--- DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateRepository.cs
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateRepository.cs
@@ -1,9 +1,7 @@
 ﻿using DatamartManagementService.Infrastructure.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DatamartManagementService.Infrastructure.Persistence
@@ -19,13 +17,31 @@ namespace DatamartManagementService.Infrastructure.Persistence
             await context.SaveChangesAsync();
         }
 
-        public async Task<List<RofRevenueByDate>> GetRevenueByYear(short year)
+        public async Task<List<RofRevenueByDate>> GetRevenueForTheYear(short year)
         {
             using var context = new RofDatamartContext();
 
             var yearlyRevenue = await context.RofRevenueByDate.Where(r => r.RevenueYear == year).ToListAsync();
 
             return yearlyRevenue;
+        }
+
+        public async Task<List<RofRevenueByDate>> GetRevenueForTheMonthOfCertainYear(short month, short year)
+        {
+            using var context = new RofDatamartContext();
+
+            var monthlyRevenue = await context.RofRevenueByDate.Where(r => r.RevenueMonth == month && r.RevenueYear == year).ToListAsync();
+
+            return monthlyRevenue;
+        }
+
+        public async Task UpdateRevenue(RofRevenueByDate updateRevenueByDate)
+        {
+            using var context = new RofDatamartContext();
+
+            context.Update(updateRevenueByDate);
+
+            await context.SaveChangesAsync();
         }
     }
 }
