c4cec46a8dac3cad070ace0c814618b41552ffb2
Becky <becky.ruan524@gmail.com>
2023-05-19 19:41:57 -0700

    revenue from service repo

---
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueFromServicesCompletedByDateRepository.cs
@@ -0,0 +1,47 @@
+﻿using DatamartManagementService.Infrastructure.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatamartManagementService.Infrastructure.Persistence
+{
+    public class RevenueFromServicesCompletedByDateRepository
+    {
+        public async Task AddRevenueFromServices(List<RofRevenueFromServicesCompletedByDate> newRevenueFromServices)
+        {
+            using var context = new RofDatamartContext();
+
+            context.RofRevenueFromServicesCompletedByDate.AddRange(newRevenueFromServices);
+
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<RofRevenueFromServicesCompletedByDate>> GetRevenueFromServicesByEmployee(long employeeId)
+        {
+            using var context = new RofDatamartContext();
+
+            var employeeRevenue = await context.RofRevenueFromServicesCompletedByDate.Where(r => r.EmployeeId == employeeId).ToListAsync();
+
+            return employeeRevenue;
+        }
+
+        public async Task<List<RofRevenueFromServicesCompletedByDate>> RevenueFromServicesByPetService(long petServiceId)
+        {
+            using var context = new RofDatamartContext();
+
+            var petServiceRevenue = await context.RofRevenueFromServicesCompletedByDate.Where(r => r.PetServiceId == petServiceId).ToListAsync();
+
+            return petServiceRevenue;
+        }
+
+        public async Task UpdateRevenueFromServices(RofRevenueFromServicesCompletedByDate updateRevenueFromServices)
+        {
+            using var context = new RofDatamartContext();
+
+            context.Update(updateRevenueFromServices);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
