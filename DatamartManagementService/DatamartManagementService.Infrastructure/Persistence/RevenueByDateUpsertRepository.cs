73b03892df7155db952c70554aa4ba18739ceccb
Becky <becky.ruan524@gmail.com>
2023-05-20 18:21:45 -0700

    removed unused using statements

--- DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateUpsertRepository.cs
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueByDateUpsertRepository.cs
@@ -1,7 +1,5 @@
 ﻿using DatamartManagementService.Infrastructure.Persistence.Entities;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatamartManagementService.Infrastructure.Persistence
--- DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueFromServicesRetrievalRepository.cs
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RevenueFromServicesRetrievalRepository.cs
@@ -1,9 +1,7 @@
 ﻿using DatamartManagementService.Infrastructure.Persistence.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DatamartManagementService.Infrastructure.Persistence
