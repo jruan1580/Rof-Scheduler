97b232f3db13ee26b8593d99fd4f4b88047152f8
Becky <becky.ruan524@gmail.com>
2023-05-24 20:39:10 -0700

    pulled rof scheduler entitles for payroll details

---
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RofSchedulerEntities/HolidayRates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities
+{
+    public partial class HolidayRates
+    {
+        public int Id { get; set; }
+        public short PetServiceId { get; set; }
+        public short HolidayId { get; set; }
+        public decimal HolidayRate { get; set; }
+
+        public virtual Holidays Holiday { get; set; }
+        public virtual PetServices PetService { get; set; }
+    }
+}
