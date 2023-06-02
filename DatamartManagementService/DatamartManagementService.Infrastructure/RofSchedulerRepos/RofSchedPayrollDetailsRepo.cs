f2b3b261ddd46b825eafb466e2f5ad66be98d998
Becky <becky.ruan524@gmail.com>
2023-06-02 09:16:29 -0700

    updated methods

--- DatamartManagementService/DatamartManagementService.Infrastructure/RofSchedulerRepos/RofSchedPayrollDetailsRepo.cs
+++ DatamartManagementService/DatamartManagementService.Infrastructure/RofSchedulerRepos/RofSchedPayrollDetailsRepo.cs
@@ -8,52 +8,53 @@ using System.Threading.Tasks;
 
 namespace DatamartManagementService.Infrastructure.RofSchedulerRepos
 {
-    //public long EmployeeId { get; set; }
-    //public string FirstName { get; set; }
-    //public string LastName { get; set; }
-    //public long PetServiceId { get; set; }
-    //public string PetServiceName { get; set; }
-    //public decimal EmployeePayForService { get; set; }
-    //public bool IsHolidayPay { get; set; }
-    //public int ServiceDuration { get; set; }
-    //public string ServiceDurationTimeUnit { get; set; }
-    //public long JobEventId { get; set; }
-    //public DateTime ServiceStartDateTime { get; set; }
-    //public DateTime ServiceEndDateTime { get; set; }
-
     public class RofSchedPayrollDetailsRepo
     {
-        //public async Task<List<Employee>> GetEmployeeDetails()
-        //{
-        //    using var context = new RofSchedulerContext();
-
-        //    return await context.Employee.Select(e => new Employee() { Id = e.Id, FirstName = e.FirstName, LastName = e.LastName }).ToListAsync();
-        //}
-
-        //public async Task<List<PetServices>> GetPetServiceDetails()
-        //{
-        //    using var context = new RofSchedulerContext();
-
-        //    return await context.PetServices.Select(s => new PetServices() { Id = s.Id, ServiceName = s.ServiceName, Duration = s.Duration, TimeUnit = s.TimeUnit }).ToListAsync();
-        //}
-
-        public async Task<List<JobEvent>> GetCompletedJobEventsOfAnEmployeeForCertainPeriod(long id, DateTime startTime, DateTime endPe)
+        public async Task<Employee> GetEmployeeById(long id)
         {
             using var context = new RofSchedulerContext();
 
-            var employeeCompletedJobEvents = await context.JobEvent.Where(j => 
-                j.EmployeeId == id 
-                && j.Completed == true 
-                && (j.EventStartTime.Month >= startTime.Month && j.EventEndTime).ToListAsync();
+            return await context.Employee.FirstOrDefaultAsync(e => e.Id == id);
         }
 
-        private bool CheckIfEventWasOnHoliday(JobEvent jobEvent)
+        public async Task<List<JobEvent>> GetCompletedServicesDoneByEmployee(long id)
         {
             using var context = new RofSchedulerContext();
 
-            var holiday = context.Holidays.FirstOrDefaultAsync(h => h.HolidayMonth == jobEvent.EventStartTime.Month && h.HolidayDay == jobEvent.EventStartTime.Day);
+            var employeeCompletedServices = await context.JobEvent.Where(j => j.EmployeeId == id 
+                && j.Completed == true).ToListAsync();
+
+            return employeeCompletedServices;
+        }
+
+        public async Task<PetServices> GetPetServiceById(short id)
+        {
+            using var context = new RofSchedulerContext();
+
+            return await context.PetServices.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<JobEvent> GetJobEventById(int id)
+        {
+            using var context = new RofSchedulerContext();
+
+            return await context.JobEvent.FirstOrDefaultAsync(j => j.Id == id);
+        }
+
+        public async Task<bool> CheckIfJobDateIsHoliday(DateTime eventDate)
+        {
+            using var context = new RofSchedulerContext();
+
+            var holiday = await context.Holidays.FirstOrDefaultAsync(h => h.HolidayMonth == eventDate.Month && h.HolidayDay == eventDate.Day);
 
             return holiday != null;
+        }
+
+        public async Task<HolidayRates> GetHolidayRateById(int id)
+        {
+            using var context = new RofSchedulerContext();
+
+            return await context.HolidayRates.FirstOrDefaultAsync(r => r.Id == id);
         }
     }
 }
