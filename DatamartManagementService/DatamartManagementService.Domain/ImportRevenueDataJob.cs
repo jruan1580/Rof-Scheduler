9f4cb0d89989ead0e961ae205da7572e985ba9fd
Jason <jruan1580@gmail.com>
2023-06-09 17:20:15 -0700

    new projects for background services

---
+++ DatamartManagementService/DatamartManagementService.Domain/ImportRevenueDataJob.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatamartManagementService.Domain
+{
+    public class ImportRevenueDataJob : BackgroundService
+    {
+        private readonly int _hoursInBetweenRun = 12;
+
+        public ImportRevenueDataJob()
+        {
+
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //keep running unless told to stop AKA told to cancel
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //TODO: import data
+                Console.WriteLine("hello from the job");
+
+                await Task.Delay(TimeSpan.FromHours(_hoursInBetweenRun), stoppingToken);
+            }       
+        }
+    }
+}
