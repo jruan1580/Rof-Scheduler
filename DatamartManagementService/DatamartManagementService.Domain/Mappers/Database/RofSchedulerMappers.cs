eae3d5321825ff65feae213fd4e72ed3b3e3d8cd
Becky <becky.ruan524@gmail.com>
2023-08-15 20:00:50 -0700

    more mappers

--- DatamartManagementService/DatamartManagementService.Domain/Mappers/Database/RofSchedulerMappers.cs
+++ DatamartManagementService/DatamartManagementService.Domain/Mappers/Database/RofSchedulerMappers.cs
@@ -2,6 +2,10 @@
 using DbJobEvent = DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities.JobEvent;
 using CoreEmployee = DatamartManagementService.Domain.Models.RofSchedulerModels.Employee;
 using DbEmployee = DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities.Employee;
+using CorePetService = DatamartManagementService.Domain.Models.RofSchedulerModels.PetServices;
+using DbPetService = DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities.PetServices;
+using CoreHolidayRate = DatamartManagementService.Domain.Models.RofSchedulerModels.HolidayRates;
+using DbHolidayRate = DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities.HolidayRates;
 
 namespace DatamartManagementService.Domain.Mappers.Database
 {
@@ -30,6 +34,32 @@ namespace DatamartManagementService.Domain.Mappers.Database
             coreEmployee.LastName = dbEmployee.LastName;
 
             return coreEmployee;
+        }
+
+        public static CorePetService ToCorePetService(DbPetService dbPetService)
+        {
+            var corePetService = new CorePetService();
+
+            corePetService.Id = dbPetService.Id;
+            corePetService.ServiceName = dbPetService.ServiceName;
+            corePetService.Price = dbPetService.Price;
+            corePetService.EmployeeRate = dbPetService.EmployeeRate;
+            corePetService.Duration = dbPetService.Duration;
+            corePetService.TimeUnit = dbPetService.TimeUnit;
+
+            return corePetService;
+        }
+
+        public static CoreHolidayRate ToCoreHolidayRate(DbHolidayRate dbHolidayRate)
+        {
+            var coreHolidayRate = new CoreHolidayRate();
+
+            coreHolidayRate.Id = dbHolidayRate.Id;
+            coreHolidayRate.HolidayId = dbHolidayRate.HolidayId;
+            coreHolidayRate.PetServiceId = dbHolidayRate.PetServiceId;
+            coreHolidayRate.HolidayRate = dbHolidayRate.HolidayRate;
+
+            return coreHolidayRate;
         }
     }
 }
