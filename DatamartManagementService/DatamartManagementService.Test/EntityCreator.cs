06ef4c0bb1889da80dcf2b2d8a2e68b152614320
Becky <becky.ruan524@gmail.com>
2023-09-16 13:47:38 -0700

    entity creator + more detailed revenue importer test

---
+++ DatamartManagementService/DatamartManagementService.Test/EntityCreator.cs
@@ -0,0 +1,79 @@
+﻿using DatamartManagementService.Infrastructure.Persistence.RofDatamartEntities;
+using DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatamartManagementService.Test
+{
+    public static class EntityCreator
+    {
+        public static JobExecutionHistory GetDbJobExecutionHistory()
+        {
+            return new JobExecutionHistory()
+            {
+                Id = 1,
+                JobType = "Revenue",
+                LastDatePulled = DateTime.Today
+            };
+        }
+
+        public static JobEvent GetDbJobEvent()
+        {
+            return new JobEvent()
+            {
+                Id = 1,
+                EmployeeId = 1,
+                PetServiceId = 1,
+                EventStartTime = DateTime.Now,
+                EventEndTime = DateTime.Now.AddHours(1),
+                Completed = true
+            };
+        }
+
+        public static Employee GetDbEmployee()
+        {
+            return new Employee()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+        }
+
+        public static PetServices GetDbPetService()
+        {
+            return new PetServices()
+            {
+                Id = 1,
+                ServiceName = "Walking",
+                Price = 25,
+                EmployeeRate = 15,
+                Duration = 1,
+                TimeUnit = "Hour"
+            };
+        }
+
+        public static Holidays GetDbHoliday()
+        {
+            return new Holidays()
+            {
+                Id = 1,
+                HolidayName = "Christmas",
+                HolidayMonth = 12,
+                HolidayDay = 25,
+            };
+        }
+
+        public static HolidayRates GetDbHolidayRates()
+        {
+            return new HolidayRates()
+            {
+                Id = 1,
+                HolidayId = 1,
+                PetServiceId = 1,
+                HolidayRate = 23
+            };
+        }
+    }
+}
--- DatamartManagementService/DatamartManagementService.Test/Importer/DetailedRevenueImporterTest.cs
+++ DatamartManagementService/DatamartManagementService.Test/Importer/DetailedRevenueImporterTest.cs
@@ -1,4 +1,5 @@
-﻿using DatamartManagementService.Infrastructure.Persistence.RofDatamartEntities;
+﻿using DatamartManagementService.Domain;
+using DatamartManagementService.Infrastructure.Persistence.RofDatamartEntities;
 using DatamartManagementService.Infrastructure.Persistence.RofDatamartRepos;
 using DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities;
 using DatamartManagementService.Infrastructure.Persistence.RofSchedulerRepos;
@@ -21,11 +22,42 @@ namespace DatamartManagementService.Test.Importer
             var detailedRevenueRepo = new Mock<IRevenueFromServicesUpsertRepository>();
             var jobExecutionHistoryRepo = new Mock<IJobExecutionHistoryRepository>();
 
+            var jobEvents = new List<JobEvent>()
+            {
+                EntityCreator.GetDbJobEvent()
+            };
+
+            var employee = EntityCreator.GetDbEmployee();
+            var petService = EntityCreator.GetDbPetService();
+
             jobExecutionHistoryRepo.Setup(j => j.GetJobExecutionHistoryByJobType(It.IsAny<string>()))
                 .ReturnsAsync((JobExecutionHistory)null);
 
             rofSchedulerRepo.Setup(r => r.GetCompletedServicesUpUntilDate(It.IsAny<DateTime>()))
-                .ReturnsAsync(new List<JobEvent>());
+                .ReturnsAsync(jobEvents);
+
+            rofSchedulerRepo.Setup(r => r.GetEmployeeById(It.IsAny<long>()))
+                .ReturnsAsync(employee);
+
+            rofSchedulerRepo.Setup(r => r.GetPetServiceById(It.IsAny<short>()))
+                .ReturnsAsync(petService);
+
+            rofSchedulerRepo.Setup(r => r.CheckIfJobDateIsHoliday(It.IsAny<DateTime>()))
+                .ReturnsAsync((Holidays)null);
+
+            //calculate revenue
+
+            detailedRevenueRepo.Setup(d => d.AddRevenueFromServices(It.IsAny<List<RofRevenueFromServicesCompletedByDate>>()))
+                .Returns(Task.CompletedTask);
+
+            jobExecutionHistoryRepo.Setup(j => j.AddJobExecutionHistory(It.IsAny<JobExecutionHistory>()))
+                .Returns(Task.CompletedTask);
+
+            var detailedRevenueImporter = new DetailedRevenueImporter(rofSchedulerRepo.Object, detailedRevenueRepo.Object, jobExecutionHistoryRepo.Object);
+
+            await detailedRevenueImporter.ImportRevenueData();
+
+            detailedRevenueRepo.Verify(d => d.AddRevenueFromServices(It.IsAny<List<RofRevenueFromServicesCompletedByDate>>()), Times.Once);
         }
     }
 }
