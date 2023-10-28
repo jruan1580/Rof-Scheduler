ff2a6b90f74e450c495c7cd63b8516eb3aee704b
Becky <becky.ruan524@gmail.com>
2023-10-27 19:21:18 -0700

    dbscaffold update employee payroll

--- DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RofDatamartEntities/RofDatamartContext.cs
+++ DatamartManagementService/DatamartManagementService.Infrastructure/Persistence/RofDatamartEntities/RofDatamartContext.cs
@@ -50,9 +50,7 @@ namespace DatamartManagementService.Infrastructure.Persistence.RofDatamartEntiti
                     .HasMaxLength(25)
                     .IsUnicode(false);
 
-                entity.Property(e => e.PayPeriodEndDate).HasColumnType("date");
-
-                entity.Property(e => e.PayPeriodStartDate).HasColumnType("date");
+                entity.Property(e => e.RevenueDate).HasColumnType("date");
             });
 
             modelBuilder.Entity<EmployeePayrollDetail>(entity =>
