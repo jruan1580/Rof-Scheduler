7a7fd40e3735bd078401af347dbc3ab3b3351d14
Becky <becky.ruan524@gmail.com>
2023-11-05 08:42:44 -0800

    dtos

---
+++ DatamartManagementService/DatamartManagementService.DTO/RofSchedulerDTO/PetServicesDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatamartManagementService.DTO.RofSchedulerDTO
+{
+    public class PetServicesDTO
+    {
+        public short Id { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal EmployeeRate { get; set; }
+
+        public int Duration { get; set; }
+
+        public string TimeUnit { get; set; }
+    }
+}
