b44df6914b1235029861e136967b1947bb716028
Becky <becky.ruan524@gmail.com>
2023-11-05 08:42:55 -0800

    controller

---
+++ DatamartManagementService/DataMart/Controller/RevenueSummaryController.cs
@@ -0,0 +1,29 @@
+﻿using DatamartManagementService.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataMart.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RevenueSummaryController : ControllerBase
+    {
+        private readonly IRevenueSummaryRetrievalService _revenueSummaryRetrievalService;
+
+        public RevenueSummaryController(IRevenueSummaryRetrievalService revenueSummaryRetrievalService)
+        {
+            _revenueSummaryRetrievalService = revenueSummaryRetrievalService;
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet]
+        public async Task<IActionResult> GetRevenueBetweenDatesByPetService([FromBody] int month, [FromBody] int year)
+        {
+            
+        }
+    }
+}
