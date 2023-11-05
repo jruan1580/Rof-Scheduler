f0805469de577202e1da757e1c1e7fa5e9808d94
Becky <becky.ruan524@gmail.com>
2023-11-05 13:43:56 -0800

    update to domain service

--- DatamartManagementService/DatamartManagementService.Test/Service/RevenueSummaryRetrievalServiceTest.cs
+++ DatamartManagementService/DatamartManagementService.Test/Service/RevenueSummaryRetrievalServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using DatamartManagementService.Domain;
 using DatamartManagementService.Infrastructure.Persistence.RofDatamartEntities;
 using DatamartManagementService.Infrastructure.Persistence.RofDatamartRepos;
+using DatamartManagementService.Infrastructure.Persistence.RofSchedulerEntities;
+using DatamartManagementService.Infrastructure.Persistence.RofSchedulerRepos;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -16,11 +18,20 @@ namespace DatamartManagementService.Test.Service
         public async Task GetRevenueBetweenDates_NoRevenueSummary()
         {
             var revenueByDateRetrievalRepo = new Mock<IRevenueByDateRetrievalRepository>();
+            var rofSchedulerRepo = new Mock<IRofSchedRepo>();
+
+            var petServices = new List<PetServices>()
+            {
+                EntityCreator.GetDbPetService()
+            };
 
             revenueByDateRetrievalRepo.Setup(r => r.GetRevenueBetweenDates(It.IsAny<DateTime>(), It.IsAny<DateTime>()))
                 .ReturnsAsync(new List<RofRevenueByDate>());
 
-            var revenueSummaryService = new RevenueSummaryRetrievalService(revenueByDateRetrievalRepo.Object);
+            rofSchedulerRepo.Setup(s => s.GetAllPetServices())
+                .ReturnsAsync(petServices);
+
+            var revenueSummaryService = new RevenueSummaryRetrievalService(revenueByDateRetrievalRepo.Object, rofSchedulerRepo.Object);
 
             var results = await revenueSummaryService.GetRevenueBetweenDatesByPetService(DateTime.Today.AddDays(-1), DateTime.Today);
 
@@ -31,27 +42,34 @@ namespace DatamartManagementService.Test.Service
         public async Task GetRevenueBetweenDates_Success()
         {
             var revenueByDateRetrievalRepo = new Mock<IRevenueByDateRetrievalRepository>();
+            var rofSchedulerRepo = new Mock<IRofSchedRepo>();
 
             var revenueSummary = new List<RofRevenueByDate>()
             {
                 EntityCreator.GetDbRevenueSummary()
             };
 
+            var petServices = new List<PetServices>()
+            {
+                EntityCreator.GetDbPetService()
+            };
+
             revenueByDateRetrievalRepo.Setup(r => r.GetRevenueBetweenDates(It.IsAny<DateTime>(), It.IsAny<DateTime>()))
                 .ReturnsAsync(revenueSummary);
 
-            var revenueSummaryService = new RevenueSummaryRetrievalService(revenueByDateRetrievalRepo.Object);
+            rofSchedulerRepo.Setup(s => s.GetAllPetServices())
+                .ReturnsAsync(petServices);
+
+            var revenueSummaryService = new RevenueSummaryRetrievalService(revenueByDateRetrievalRepo.Object, rofSchedulerRepo.Object);
 
             var results = await revenueSummaryService.GetRevenueBetweenDatesByPetService(DateTime.Today.AddDays(-1), DateTime.Today);
 
             Assert.IsNotNull(results);
             Assert.AreEqual(1, results.Count);
-            Assert.AreEqual(1, results[1].Count);
-            Assert.AreEqual(DateTime.Today, results[1][0].RevenueDate);
-            Assert.AreEqual(DateTime.Today.Month, results[1][0].RevenueMonth);
-            Assert.AreEqual(DateTime.Today.Year, results[1][0].RevenueYear);
-            Assert.AreEqual(20, results[1][0].GrossRevenue);
-            Assert.AreEqual(5, results[1][0].NetRevenuePostEmployeePay);
+            Assert.AreEqual("Walking", results[0].PetService.ServiceName);
+            Assert.AreEqual(1, results[0].Count);
+            Assert.AreEqual(20, results[0].GrossRevenuePerService);
+            Assert.AreEqual(5, results[0].NetRevenuePerService);
         }
     }
 }
