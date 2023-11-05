d81c5fa75ca5dec5f210b801f7f2c27199a47893
Becky <becky.ruan524@gmail.com>
2023-11-05 11:51:29 -0800

    dto mappers

---
+++ DatamartManagementService/DatamartManagementService.Domain/Mappers/DTO/PetServicesDTOMapper.cs
@@ -0,0 +1,36 @@
+﻿using DatamartManagementService.DTO.RofSchedulerDTO;
+using CorePetService = DatamartManagementService.Domain.Models.RofSchedulerModels.PetServices;
+
+namespace DatamartManagementService.Domain.Mappers.DTO
+{
+    public static class PetServicesDTOMapper
+    {
+        public static PetServicesDTO ToDTOPetServices(CorePetService corePetService)
+        {
+            var dtoPetService = new PetServicesDTO();
+
+            dtoPetService.Id = corePetService.Id;
+            dtoPetService.ServiceName = corePetService.ServiceName;
+            dtoPetService.EmployeeRate = corePetService.EmployeeRate;
+            dtoPetService.Price = corePetService.Price;
+            dtoPetService.Duration = corePetService.Duration;
+            dtoPetService.TimeUnit = corePetService.TimeUnit;
+
+            return dtoPetService;
+        }
+
+        public static CorePetService FromDTOPetServices(PetServicesDTO dtoPetService)
+        {
+            var corePetService = new CorePetService();
+
+            corePetService.Id = dtoPetService.Id;
+            corePetService.ServiceName = dtoPetService.ServiceName;
+            corePetService.EmployeeRate = dtoPetService.EmployeeRate;
+            corePetService.Price = dtoPetService.Price;
+            corePetService.Duration = dtoPetService.Duration;
+            corePetService.TimeUnit = dtoPetService.TimeUnit;
+
+            return corePetService;
+        }
+    }
+}
---
+++ DatamartManagementService/DatamartManagementService.Domain/Mappers/DTO/RofRevenueByDateDTOMapper.cs
@@ -0,0 +1,38 @@
+﻿using DatamartManagementService.DTO.RofDatamartDTO;
+using CoreRevenueSummary = DatamartManagementService.Domain.Models.RofDatamartModels.RofRevenueByDate;
+
+namespace DatamartManagementService.Domain.Mappers.DTO
+{
+    public static class RofRevenueByDateDTOMapper
+    {
+        public static RofRevenueByDateDTO ToDTORevenueSummary(CoreRevenueSummary coreRevenueSummary)
+        {
+            var dtoRevenueSummary = new RofRevenueByDateDTO();
+
+            dtoRevenueSummary.Id = coreRevenueSummary.Id;
+            dtoRevenueSummary.PetServiceId = coreRevenueSummary.PetServiceId;
+            dtoRevenueSummary.RevenueDate = coreRevenueSummary.RevenueDate;
+            dtoRevenueSummary.RevenueMonth = coreRevenueSummary.RevenueMonth;
+            dtoRevenueSummary.RevenueYear = coreRevenueSummary.RevenueYear;
+            dtoRevenueSummary.GrossRevenue = coreRevenueSummary.GrossRevenue;
+            dtoRevenueSummary.NetRevenuePostEmployeePay = coreRevenueSummary.NetRevenuePostEmployeePay;
+
+            return dtoRevenueSummary;
+        }
+
+        public static CoreRevenueSummary FromDTORevenueSummary(RofRevenueByDateDTO dtoRevenueSummary)
+        {
+            var coreRevenueSummary = new CoreRevenueSummary();
+
+            coreRevenueSummary.Id = dtoRevenueSummary.Id;
+            coreRevenueSummary.PetServiceId = dtoRevenueSummary.PetServiceId;
+            coreRevenueSummary.RevenueDate = dtoRevenueSummary.RevenueDate;
+            coreRevenueSummary.RevenueMonth = dtoRevenueSummary.RevenueMonth;
+            coreRevenueSummary.RevenueYear = dtoRevenueSummary.RevenueYear;
+            coreRevenueSummary.GrossRevenue = dtoRevenueSummary.GrossRevenue;
+            coreRevenueSummary.NetRevenuePostEmployeePay = dtoRevenueSummary.NetRevenuePostEmployeePay;
+
+            return coreRevenueSummary;
+        }
+    }
+}
