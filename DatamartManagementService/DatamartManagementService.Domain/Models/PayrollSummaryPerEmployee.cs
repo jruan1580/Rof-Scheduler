18feaee54384de3a07e76416fd8e7cf4026f1052
Becky <becky.ruan524@gmail.com>
2024-01-14 22:10:36 -0800

    get payroll summary w/ pages domain method

---
+++ DatamartManagementService/DatamartManagementService.Domain/Models/PayrollSummaryPerEmployee.cs
@@ -0,0 +1,16 @@
+﻿namespace DatamartManagementService.Domain.Models
+{
+    public class PayrollSummaryPerEmployee
+    {
+        public PayrollSummaryPerEmployee(string firstName, string lastName, decimal totalPay)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TotalPay = totalPay;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal TotalPay { get; set; }
+    }
+}
