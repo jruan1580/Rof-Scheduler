508f914c9ede3256cfcb5a33c5d9c73f3ff0f645
Becky <becky.ruan524@gmail.com>
2024-01-17 18:06:27 -0800

    unit test

---
+++ DatamartManagementService/DatamartManagementService.Test/Service/PayrollSummaryRetrievalServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatamartManagementService.Test.Service
+{
+    [TestFixture]
+    public class PayrollSummaryRetrievalServiceTest
+    {
+
+    }
+}
