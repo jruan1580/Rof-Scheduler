45bf4af76661fa8abc50a312e2bf97cb0e251665
Becky <becky.ruan524@gmail.com>
2024-01-29 19:39:34 -0800

    unit tests

--- DatamartManagementService/DatamartManagementService.Test/Service/PayrollSummaryRetrievalServiceTest.cs
+++ DatamartManagementService/DatamartManagementService.Test/Service/PayrollSummaryRetrievalServiceTest.cs
@@ -18,7 +18,12 @@ namespace DatamartManagementService.Test.Service
         {
             var payrollRetrievalRepo = new Mock<IPayrollRetrievalRepository>();
 
-            payrollRetrievalRepo.Setup(p => p.GetEmployeePayrollBetweenDatesByEmployee(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+            payrollRetrievalRepo.Setup(p => 
+                p.GetEmployeePayrollBetweenDatesByEmployee(
+                        It.IsAny<string>(), 
+                        It.IsAny<string>(), 
+                        It.IsAny<DateTime>(), 
+                        It.IsAny<DateTime>()))
                 .ReturnsAsync(new List<EmployeePayroll>());
 
             var payrollSummaryService = new PayrollSummaryRetrievalService(payrollRetrievalRepo.Object);
@@ -26,6 +31,38 @@ namespace DatamartManagementService.Test.Service
             var results = await payrollSummaryService.GetPayrollSummary("Peter", "Piper", DateTime.Today.AddDays(-1), DateTime.Today);
 
             Assert.IsEmpty(results.PayrollSummaryPerEmployee);
+            Assert.AreEqual(0, results.TotalPages);
+        }
+
+        [Test]
+        public async Task GetPayrollSummary_Success()
+        {
+            var payrollRetrievalRepo = new Mock<IPayrollRetrievalRepository>();
+
+            var payrollSummary = new List<EmployeePayroll>()
+            {
+                EntityCreator.GetDbPayrollSummary()
+            };
+
+            payrollRetrievalRepo.Setup(p =>
+                p.GetEmployeePayrollBetweenDatesByEmployee(
+                        It.IsAny<string>(),
+                        It.IsAny<string>(),
+                        It.IsAny<DateTime>(),
+                        It.IsAny<DateTime>()))
+                .ReturnsAsync(payrollSummary);
+
+            var payrollSummaryService = new PayrollSummaryRetrievalService(payrollRetrievalRepo.Object);
+
+            var results = await payrollSummaryService.GetPayrollSummary("John", "Doe", DateTime.Today.AddDays(-1), DateTime.Today);
+
+            Assert.IsNotEmpty(results.PayrollSummaryPerEmployee);
+            Assert.AreEqual(1, results.TotalPages);
+
+            Assert.IsNotNull(results.PayrollSummaryPerEmployee[0]);
+            Assert.AreEqual("John", results.PayrollSummaryPerEmployee[0].FirstName);
+            Assert.AreEqual("Doe", results.PayrollSummaryPerEmployee[0].LastName);
+            Assert.AreEqual(20, results.PayrollSummaryPerEmployee[0].TotalPay);
         }
     }
 }
