7e0a6d87a853af1678e229b8e224f5996e9df780
Becky <becky.ruan524@gmail.com>
2024-01-30 14:20:34 -0800

    unit test

--- DatamartManagementService/DatamartManagementService.Test/Service/PayrollSummaryRetrievalServiceTest.cs
+++ DatamartManagementService/DatamartManagementService.Test/Service/PayrollSummaryRetrievalServiceTest.cs
@@ -64,5 +64,34 @@ namespace DatamartManagementService.Test.Service
             Assert.AreEqual("Doe", results.PayrollSummaryPerEmployee[0].LastName);
             Assert.AreEqual(20, results.PayrollSummaryPerEmployee[0].TotalPay);
         }
+
+        [Test]
+        public async Task GetPayrollSummary_NoNameProvidedSuccess()
+        {
+            var payrollRetrievalRepo = new Mock<IPayrollRetrievalRepository>();
+
+            var payrollSummary = new List<EmployeePayroll>()
+            {
+                EntityCreator.GetDbPayrollSummary()
+            };
+
+            payrollRetrievalRepo.Setup(p =>
+                p.GetEmployeePayrollBetweenDatesByEmployee(
+                        It.IsAny<string>(),
+                        It.IsAny<string>(),
+                        It.IsAny<DateTime>(),
+                        It.IsAny<DateTime>()))
+                .ReturnsAsync(payrollSummary);
+
+            var payrollSummaryService = new PayrollSummaryRetrievalService(payrollRetrievalRepo.Object);
+
+            var results = await payrollSummaryService.GetPayrollSummary("", "", DateTime.Today.AddDays(-1), DateTime.Today);
+
+            Assert.IsNotEmpty(results.PayrollSummaryPerEmployee);
+            Assert.AreEqual(1, results.TotalPages);
+
+            Assert.IsNotNull(results.PayrollSummaryPerEmployee[0]);
+            Assert.AreEqual(20, results.PayrollSummaryPerEmployee[0].TotalPay);
+        }
     }
 }
