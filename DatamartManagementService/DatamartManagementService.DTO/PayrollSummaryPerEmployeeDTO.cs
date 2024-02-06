5e73b152ac2cbe8ab74a960dd31c579c6716aa8b
Becky <becky.ruan524@gmail.com>
2024-02-05 16:41:44 -0800

    payroll summary retrieval controller

---
+++ DatamartManagementService/DatamartManagementService.DTO/PayrollSummaryPerEmployeeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatamartManagementService.DTO
+{
+    public class PayrollSummaryPerEmployeeDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public decimal TotalPay { get; set; }
+    }
+}
