625466ee3c1bf13e205d47a49d85bed5b48fcc19
Becky <becky.ruan524@gmail.com>
2024-02-20 11:02:51 -0800

    added get payroll sum method

--- rof-scheduler/src/SharedServices/datamartService.js
+++ rof-scheduler/src/SharedServices/datamartService.js
@@ -6,3 +6,11 @@ export const getRevenueBetweenDatesByPetService = async function (startDate, end
 
   return await makeHttpRequest(url, "GET", { "Accept": "application/json"}, 200, undefined);  
 };
+
+export const getPayrollBetweenDatesByEmployee = async function (firstName, lastName, startDate, endDate, page) {
+  var baseUrl = process.env.REACT_APP_DATAMART_SERVICE_BASE_URL;
+  var data = { firstName, lastName, startDate, endDate, page }
+  var url = baseUrl + "/payrollSummary";
+
+  return await makeHttpRequest(url, "GET", {"Content-Type": "application/json"}, 200, data);  
+};
